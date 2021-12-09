bee222c4095d2522972f62e6ad9cd65a69342b04
sivaaks <senthilsivaaks@gmail.com>
2021-12-09 21:14:17 +0530

    ssl certificate added

--- src/Utils.js
+++ src/Utils.js
@@ -5,6 +5,6 @@ const API_FORGOT_PASSWORD='https://siva-url-shortner.herokuapp.com/users/forgot-
 const API_RESET_PASSWORD='https://siva-url-shortner.herokuapp.com/users/reset-password';
 const API_URL='https://siva-url-shortner.herokuapp.com/urls';
 
-const SITE_ADDRESS='http://reus.ml';
+const SITE_ADDRESS='https://reus.ml';
 
 export {API_REGISTER,API_LOGIN,API_FORGOT_PASSWORD,API_RESET_PASSWORD,API_URL,SITE_ADDRESS};
