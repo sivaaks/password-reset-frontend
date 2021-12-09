b7c4957fd78506e041b8475e6e87935950faef3a
sivaaks <senthilsivaaks@gmail.com>
2021-12-09 21:08:01 +0530

    url changed

--- src/Utils.js
+++ src/Utils.js
@@ -1,8 +1,8 @@
 
 const API_REGISTER='https://siva-url-shortner.herokuapp.com/users/register';
-const API_LOGIN='https://siva-url-shortner.herokuapp.com/login';
-const API_FORGOT_PASSWORD='https://siva-url-shortner.herokuapp.com/forgot-password';
-const API_RESET_PASSWORD='https://siva-url-shortner.herokuapp.com/reset-password';
+const API_LOGIN='https://siva-url-shortner.herokuapp.com/users/login';
+const API_FORGOT_PASSWORD='https://siva-url-shortner.herokuapp.com/users/forgot-password';
+const API_RESET_PASSWORD='https://siva-url-shortner.herokuapp.com/users/reset-password';
 const API_URL='https://siva-url-shortner.herokuapp.com/urls';
 
 const SITE_ADDRESS='http://reus.ml';
